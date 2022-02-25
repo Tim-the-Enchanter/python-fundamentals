58f9c91af27af22a31553b2fc1e56e057e101007
Tim Donnelley <sgtpixel@gmail.com>
2022-02-25 16:39:05 -0600

    Updated assignment 2-11, 2-1, 2-2 need to add 8-1 and 8-2

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,1 +1,44 @@
+# Recieved error message
+# PEP 8: W292 no newline at end of file
+# letting me know that there was no new line.
+# Also, hitting "enter" or "return" in the middle of a sentence intuitively added a hash mark.
+
 print("Hello World")
+
+# Try It yourself 2-11 >import this returned :
+# The Zen of Python, by Tim Peters
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
+
+# added variable "message"
+# added print function to display "Hello Python Classroom" value as output
+message = "Hello Python Classroom!"
+print(message)
+
+# changed value "Hello World" to "Hello Python World"
+# added print function to display "Hello Python World" value as output
+print("Hello Python World")
+
+# added variable "affirm"
+# added print function to display "Roger Roger" value as output
+affirm = "Roger Roger!"
+print(affirm)
+
