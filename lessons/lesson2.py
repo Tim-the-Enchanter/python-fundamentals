af8de443ccb075776cc194f08b3a0c854bf2bf96
Tim Donnelley <sgtpixel@gmail.com>
2022-02-28 04:51:08 -0600

    Lesson 2 file created

---
+++ lessons/lesson2.py
@@ -0,0 +1,23 @@
+# My Application Overview Lesson
+
+# This function should be lowercase and if more than one word it
+# should have an underscore to separate each word. You should have
+# a parenthesis after the name followed by a colon. Statements that are
+# part of the function should be 4 spaces indented.
+def my_function():
+    print('Hello')
+    print('World')
+
+
+# To run the function, you must call it by name.
+my_function()
+
+# When defining a function with arguments, those arguments go in beteween
+# the parenthesis and separated by commas.
+def my_other(arg1, arg2):
+    print(arg1)
+    print(arg2)
+
+
+my_other('Red', 'Green')
+
