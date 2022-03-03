d637464eb583e0dd4bba049196ce18fc4947d01e
Tim Donnelley <sgtpixel@gmail.com>
2022-03-03 08:02:44 -0600

    CAC-102-2 Lesson 2 My Appllication Overview

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -22,3 +22,55 @@ def my_other(arg1, arg2):
 
 my_other('Red', 'Green')
 
+# Variable Names
+# - Must start with a letter or and underscore
+# - Can not begin with a number
+# - Can only contain alphanumeric characters and underscores
+# - Are case-sensitive
+
+# The variables are on the left while thr literal is on the right
+# value & value2 are variables
+# blue and 10 are literals
+# combined they are a field
+value = 'Blue'
+value2 = 10
+print(value)
+print(value2)
+
+# Variables can even change types, although you may want to avoid this
+value3 = 'Happy'
+print(value3)
+value3 = 20
+print(value3)
+
+
+# Multiline statements use a backslash to continue a statement on a second line.
+alpha = 1 + 2 + 3 \
+    + 4 + 5
+print(alpha)
+
+# More than one variable on the same line
+beta = 10; charlie = "python"; delta = 5
+
+up, down, left = "town", "stairs", "right"
+print(up)
+print(down)
+print(left)
+
+
+# A class is an object which is a blueprint. An instantiation of an object
+# allows for all fields and functions to be accessed within. Like funstions
+# outside the class, you need to provide 2 empty spaces before its defined
+# Basic class example
+class MyFirstClass:
+    name= 'Tim'
+
+    def something(self):
+        print('something')
+
+
+my_class = MyFirstClass()
+print(my_class.name)
+my_class.something()
+
+
