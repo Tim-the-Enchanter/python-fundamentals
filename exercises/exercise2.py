c01ae86667add0b8c0c932f4cb4774c463df79cd
Tim Donnelley <sgtpixel@gmail.com>
2022-03-06 05:17:22 -0600

    Updated assignment 2-11, 2-1, 2-2 added 8-1 and 8-2

---
+++ exercises/exercise2.py
@@ -0,0 +1,37 @@
+# Exercise 2 Functions and Variables
+# Part 1 - Write a function called simple():
+# Assign a different message to 5 variables and print each message.
+
+def simple():
+    """Display a simple message"""
+    print("Niall")
+    print("Nola")
+    print("Kennah")
+    print("Emma")
+    print("Kyndall")
+
+
+simple()
+
+
+def simple2():
+    """Display a simple message"""
+    print("Blue")
+    print("Yellow")
+    print("Green")
+    print("Purple")
+
+
+simple2()
+
+
+def message(arg1):
+    print(arg1)
+
+
+message('Kids and Colors!')
+
+
+
+
+
