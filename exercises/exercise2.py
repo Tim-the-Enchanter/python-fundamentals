03ccf8c97fac663a3f7e8467584b099dd8c7c736
Tim Donnelley <sgtpixel@gmail.com>
2022-03-06 05:22:12 -0600

    CAC-102-2 Exercise 2
    Lab Exercise – My Application Overview

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -14,6 +14,11 @@ def simple():
 simple()
 
 
+# Part 2 - Write a function called simple2(). Assign a message to a variable, then print out that variable.
+# Change the message and assign it to the variable again, but after the first print statement. Print
+# the second message. Do these steps 2 more times. You should have 4 messages assigned to the
+# same variable and 4 print functions showing the results.
+
 def simple2():
     """Display a simple message"""
     print("Blue")
@@ -24,6 +29,9 @@ def simple2():
 
 simple2()
 
+
+# Part 3 - Write a function called message that takes 1 argument. Inside that function, write a print
+# function that takes the argument.
 
 def message(arg1):
     print(arg1)
